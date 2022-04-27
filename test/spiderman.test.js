645e6e2f4e6e0692bf62ab9fa303ae0b39650f24
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-26 21:07:56 -0500

    test: re-writing dummy test to spiderman test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test Suite Dummy Description", () => {
-    test("Case 1 Dummy", () =>{
-        const resultOfSomething = 1 + 2;
-        expect(resultOfSomething).toBe(10);
+describe("Unit Tests for spiderman class", () => {
+    test("Create an spiderman object", () =>{
+        //Código a utilizar
+        //Busco instanciar un objeto con esta información.
+        //¿Qué debo hacer para obtener dicho resultado?
+
+        const TobeyMcGuire = new Spiderman("Spiderman original", 40, "Tobey Mc. Guire", 2, "Sony");
+
+        //Valido que el código funcione como espero
+        expect(TobeyMcGuire.name).toBe("Spiderman original");
+        expect(TobeyMcGuire.age).toBe(46);
+        expect(TobeyMcGuire.actor).toBe("Tobey Mc. Guire");
+        expect(TobeyMcGuire.movies).toBe(4);
+        expect(TobeyMcGuire.studio).toBe("Sony");
     });
 });
